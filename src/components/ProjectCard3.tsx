3b4842c77d3aea5bea258171ab7194a6d490d33f
mabelzhou <135676782+mabelzhou@users.noreply.github.com>
2025-07-06 17:01:03 -0400

    updated projects page

---
+++ src/components/ProjectCard3.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Project } from '../pages/Projects'
+import { Github } from 'react-bootstrap-icons'
+import { ExternalLink } from 'lucide-react'
+import "./ProjectCard3.css"
+
+const ProjectCard3 = (project: Project) => (
+  <div className="project-card3">
+    <div className="project-card3__image-container">
+      <img src={project.image} alt={project.name} className="project-card3__image" />
+    </div>
+
+    <div className="project-card3__content">
+      <div className="project-card3__tags">
+        {project.tags.map((tag) => (
+          <span key={tag} className="project-card3__tag">
+            {tag}
+          </span>
+        ))}
+      </div>
+
+      <h3 className="project-card3__title">{project.name}</h3>
+      <p className="project-card3__description">{project.description}</p>
+
+      <div className="project-card3__links">
+        {project.link && (
+          <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-card3__link" title="Live Demo">
+            <ExternalLink size={20} />
+          </a>
+        )}
+        {project.github && (
+          <a href={project.github} target="_blank" rel="noopener noreferrer" className="project-card3__link" title="GitHub Repo">
+            <Github size={20} />
+          </a>
+        )}
+      </div>
+    </div>
+  </div>
+);
+
+export default ProjectCard3;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,3 @@
-import ProjectCard2 from "../components/ProjectCard2";
 import "./Projects.css";
 import diceImg from "../assets/dice.png";
 import pathfindingImg from "../assets/pathfinder.png";
@@ -7,11 +6,129 @@ import shirtImg from "../assets/shirt.png";
 import weatherImg from "../assets/weather.png";
 import websiteImg from "../assets/website.png";
 import spiceforgeImg from "../assets/spiceforge.png";
+import snaptrackImg from "../assets/snaptrack.png";
+import ProjectCard3 from "../components/ProjectCard3";
+import airbnbImg from "../assets/airbnb.png";
+
+export type Project = {
+  image: string;
+  name: string;
+  description: string;
+  link: string;
+  github: string;
+  demo: boolean;
+  tags: string[];
+}
 
 const Projects = () => {
+  const projects: Project[] = [
+    {
+      image: snaptrackImg,
+      name: "SnapTrack",
+      description: "A web application for tracking and managing your daily tasks and habits with AI integration",
+      link: "https://snaptrack-gold.vercel.app/",
+      github: "https://github.com/mabelzhou/snaptrack",
+      demo: true,
+      tags: ["React", "Next.js", "TailwindCSS", "Gemini AI", "Clerk", "Supabase", "Arcjet", "TypeScript"]
+    },
+    {
+      image: spiceforgeImg,
+      name: "Spice Forge",
+      description: "A website for a meal kit delivery service using EJS and Express.",
+      link: "https://spice-forge.onrender.com/",
+      github: "https://github.com/mabelzhou/spice-forge",
+      demo: true,
+      tags: ["JavaScript", "EJS", "Express", "Node.js", "MongoDB", "Mailgun"]
+    },
+    {
+      image: airbnbImg,
+      name: "Airbnb App Clone",
+      description: "A frontend clone of the Airbnb app using React Native and Expo.",
+      link: "",
+      github: "https://github.com/mabelzhou/airbnb-clone",
+      demo: false,
+      tags: ["React Native", "TypeScript", "Expo", "Clerk", "iOS", "Android", "Geodata"]
+    },
+    {
+      image: websiteImg,
+      name: "Personal Website",
+      description: "You're here now! Welcome, and thanks for visiting! 😊",
+      link: "",
+      github: "https://github.com/mabelzhou/mabelzhou.github.io",
+      demo: true,
+      tags: ["React", "Vite", "React Router", "TypeScript"]
+    },
+    {
+      image: weatherImg,
+      name: "Weather App",
+      description: "A weather app made using React and the OpenWeatherMap API. It displays the current weather and a 3-day forecast for any city.",
+      link: "https://mabelzhou.github.io/weather-app/",
+      github: "https://www.github.com/mabelzhou/weather-app",
+      demo: true,
+      tags: ["React", "API", "TypeScript", "Vite"]
+    },
+    {
+      image: diceImg,
+      name: "Dice Roller",
+      description: "A web-based virtual dice roller made using Three.js for 3D rendering and Cannon-es for physics simulation.",
+      link: "https://mabelzhou.github.io/dice-roller/",
+      github: "https://www.github.com/mabelzhou/dice-roller",
+      demo: true,
+      tags: ["Three.js", "Cannon-es", "WebGL", "JavaScript"]
+    },
+    // {
+    //   image: "",
+    //   name: "Kids First",
+    //   description: "A volunteer made co-parenting app that aspires to give divorced parents a fair and practical tool to schedule child custody without conflict",
+    //   link: "",
+    //   github: "https://github.com/JULIERAJ/KIDS-FIRST-v2",
+    //   demo: false,
+    //   tags: ["React", "TypeScript", "Firebase"]
+    // },
+    {
+      image: qrImg,
+      name: "QR Code Generator",
+      description: "A QR code generator made using React. It generates a QR code for any text inputted by the user with customizable colours.",
+      link: "https://mabelzhou.github.io/qr-code-generator/",
+      github: "https://www.github.com/mabelzhou/qr-code-generator",
+      demo: true,
+      tags: ["React", "QR", "Vite", "TypeScript", "API", "Node.js"]
+    },
+    
+    {
+      image: pathfindingImg,
+      name: "Pathfinding Visualizer",
+      description: "A pathfinding visualizer written in Python using Pygame. It uses the A* algorithm to find the shortest path between two points.",
+      link: "",
+      github: "https://www.github.com/mabelzhou/pathfinding-visualizer",
+      demo: false,
+      tags: ["Python", "Pygame", "Visualization", "Algorithms"]
+    },
+    {
+      image: shirtImg,
+      name: "Shirt Customizer",
+      description: "A shirt customizer made using React and Tailwind. It allows the user to customize the shirt's colour, texture, and logo.",
+      link: "https://mabelzhou.github.io/shirt-customizer/",
+      github: "https://www.github.com/mabelzhou/shirt-customizer",
+      demo: true,
+      tags: ["React", "TailwindCSS", "Three.js", "JavaScript", "Vite"]
+    }
+  ];
+
   return (
     <>
-      <div className="card-container">
+      <section id="projects">
+        <div className="outer-wrapper"> 
+          <div className="card-container">
+            {projects.map((project, index) => (
+              <ProjectCard3 key={index} {...project} />
+            ))}
+          </div>
+        </div>
+      </section>
+
+
+      {/* <div className="card-container">
         <ProjectCard2
           image={spiceforgeImg}
           name="Spice Forge"
@@ -68,7 +185,8 @@ const Projects = () => {
           github="https://www.github.com/mabelzhou/shirt-customizer"
           demo={true}
         />
-      </div>
+      </div> */}
+      
     </>
   );
 };
